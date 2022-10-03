09bf520813f2524925e126572fa03ef5c7fe44fa
mcjlnrtwcz <mcjlnrtwcz@users.noreply.github.com>
2022-10-03 09:29:33 +0000

    Reusable component - template approach

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 (() => {
+  /* Main video player
+  ============================================================ */
+
   const [video] = document.getElementsByClassName('player__video');
   const playpause = document.getElementById('playpause');
 
@@ -27,4 +30,18 @@
   playpause.addEventListener('click', () => {
     video.paused || video.ended ? video.play() : video.pause();
   });
+
+  /* Components
+  ============================================================ */
+  const parser = new DOMParser();
+  fetch('component.html').then((response) =>
+    response.text().then((text) => {
+      const parsedDocument = parser.parseFromString(text, 'text/html');
+      const component = parsedDocument.getElementById('component');
+      const targetElement = document.createElement('div');
+      // Clone the template content to reuse it multiple times
+      targetElement.append(component.content.cloneNode(true));
+      document.body.append(targetElement);
+    })
+  );
 })();
