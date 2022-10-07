27ecf5e696b72217af92763bde69d6f989c2be8e
mcjlnrtwcz <mcjlnrtwcz@users.noreply.github.com>
2022-10-07 08:10:50 +0000

    Scroll to one project - example

--- components/my-projects/my-projects.js
+++ components/my-projects/my-projects.js
@@ -1,3 +1,5 @@
 export default function (root) {
-  // Empty
+  const button6 = root.getElementById('project-6-button');
+  const project6 = root.getElementById('project-6');
+  button6.onclick = () => project6.scrollIntoView();
 }
