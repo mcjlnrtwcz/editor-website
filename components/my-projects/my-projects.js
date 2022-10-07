bbc3916232df558683688427483d5ce38f4e42ca
mcjlnrtwcz <mcjlnrtwcz@users.noreply.github.com>
2022-10-07 09:58:21 +0000

    Use templates within components

--- components/my-projects/my-projects.js
+++ components/my-projects/my-projects.js
@@ -1,5 +1,23 @@
 export default function (root) {
-  const button6 = root.getElementById('project-6-button');
-  const project6 = root.getElementById('project-6');
-  button6.onclick = () => project6.scrollIntoView();
+  const data = JSON.parse(root.getElementById('data').text);
+  console.log({ data });
+
+  // TODO: If you know "projects__content", can you get template automatically? const {element, template} = getELementAndTemplate();
+  const projects = root.getElementById('projects__content');
+  const nav = root.getElementById('projects__nav');
+
+  const contentTemplate = root.getElementById('projects__content__template');
+  const navTemplate = root.getElementById('projects__nav__template');
+
+  data.forEach(() => {
+    const content = contentTemplate.content.cloneNode(true);
+    projects.append(content);
+
+    const navItem = navTemplate.content.cloneNode(true);
+    nav.append(navItem);
+  });
+
+  // const button6 = root.getElementById('project-6-button');
+  // const project6 = root.getElementById('project-6');
+  // button6.onclick = () => project6.scrollIntoView();
 }
